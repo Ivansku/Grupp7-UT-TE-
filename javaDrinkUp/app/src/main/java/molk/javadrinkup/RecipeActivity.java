62a72b29a40effed7d0c79282ead49aa9c2095c4
Ivansku <Ivan.Zarkov@live.se>
2014-11-12 18:38:24 +0100

    Implementing Layouts and Buttons

--- javaDrinkUp/app/src/main/java/molk/javadrinkup/RecipeActivity.java
+++ javaDrinkUp/app/src/main/java/molk/javadrinkup/RecipeActivity.java
@@ -5,21 +5,20 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-//hejhej
-//test jasmin
-public class MainActivity extends Activity {
+
+public class RecipeActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_recipe);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_recipe, menu);
         return true;
     }
 
@@ -37,5 +36,4 @@ public class MainActivity extends Activity {
 
         return super.onOptionsItemSelected(item);
     }
-
 }
