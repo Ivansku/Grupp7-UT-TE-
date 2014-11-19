490d1501655aea26b556621b7a07640ca3f614fa
Ivansku <Ivan.Zarkov@live.se>
2014-11-19 14:47:01 +0100

    List of Drinks now loads on MainStart

--- javaDrinkUp/app/src/main/java/molk/javadrinkup/DrinksActivity.java
+++ javaDrinkUp/app/src/main/java/molk/javadrinkup/DrinksActivity.java
@@ -1,6 +1,7 @@
 package molk.javadrinkup;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,20 +11,24 @@ import android.widget.ArrayAdapter;
 import android.widget.GridView;
 import android.widget.ListView;
 import android.widget.Toast;
-
+import android.widget.AdapterView.OnItemSelectedListener;
 import java.util.ArrayList;
 
 
 public class DrinksActivity extends Activity {
+    ListView listView;
     ArrayList<String> listItems;
-
+    View view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_drinks);
+        ListDrinks(listView);
+
     }
 //Method for list
     public void ListDrinks(View view) {
+
         listItems =new ArrayList<String>();
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line, listItems);
         //Adds Items in ListView-list.
@@ -35,30 +40,39 @@ public class DrinksActivity extends Activity {
         listItems.add("Rom & Cola");
         listItems.add("Gin & Tonic");
         //Print list
-        ListView listView = (ListView) findViewById(R.id.drinksListView);
+        listView = (ListView) findViewById(R.id.drinksListView);
         listView.setAdapter(adapter);
-        HandleDrinksClick(view);
+        //onListItemClick(listView,listView,0,R.id.drinksListView);
+
+
+
+
 
 
     }
-        public void HandleDrinksClick(View view){
-            if (listItems.contains("Rom & Cola")){
-                //Toast.makeText(this,"Staropramen buttom pressed", Toast.LENGTH_SHORT).show();
-            }
-        }
+
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        String item = (String) listView.getAdapter().getItem(position);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+
+    }
+
+
     @Override
     protected void onStart()
     {
+
         // TODO Auto-generated method stub
         super.onStart();
 
-        
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_drinks, menu);
+
         return true;
     }
 
@@ -68,6 +82,7 @@ public class DrinksActivity extends Activity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
+
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
